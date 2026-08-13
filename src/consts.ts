3ac2139a8c41603f7df01b618eac3da33383d3c3
Hans Henrik Grønsleth <hei@hanshenrik.com>
2026-08-13 20:59:46 +0200

    initial commit, astro template

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Lofoten Jazzklubb";
+export const SITE_DESCRIPTION = "Jazz i Svolvær, Kabelvåg og Henningsvær";
